10ba8845bc7a2409de90af2696e8816cb4bddf7c
Timon Riemslagh <timon.riemslagh@wijs.be>
2018-08-03 21:46:13 +0200

    Init.

---
+++ js/theme.concat.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    console.log('test');
+});
+$(document).ready(function() {
+    console.log('test');
+});
+$(document).ready(function() {
+    console.log('test');
+});
+$(document).ready(function() {
+    console.log('test');
+});
+
+
+
+
+
+
+
+
